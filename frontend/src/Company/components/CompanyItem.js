3ffe697aa573da901fdefa9b79b581b6ac30ce05
sapir <sapir@almond-qms,com>
2024-05-29 22:36:32 +0300

    compniesList page with bootstrap

--- frontend/src/Company/components/CompanyItem.js
+++ frontend/src/Company/components/CompanyItem.js
@@ -1,20 +1,13 @@
 import React from "react";
 
-import './CompanyItem.css';
-
-const CompanyItem = props => {
-    return (
-        <li className="company-item">
-            <div className="company-item__content">
-                <div className="company-item__info">
-                    <h2>{props.name}</h2>
-                    <h3>
-                        {props.printersCount} {props.printersCount === 1 ? "Printer" : "Printers"}
-                    </h3>
-                </div>
-            </div>
-        </li>
-    )
+const CompanyItem = (props) => {
+  return (
+    <tr>
+      <td>{props.id}</td>
+      <td>{props.name}</td>
+      <td>{props.printersCount}</td>
+    </tr>
+  );
 };
 
 export default CompanyItem;
