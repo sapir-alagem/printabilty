302718e39e680c065cf23d78627c0acd56a1d455
idantulu <114847264+idanmeyer79@users.noreply.github.com>
2024-08-14 15:27:16 +0300

    chore: Update email service to include attachments and improve code readability

--- backend/services/email_service.js
+++ backend/services/email_service.js
@@ -1,28 +1,39 @@
-const nodeMailer = require('nodemailer');
-
+const nodeMailer = require("nodemailer");
 const html = (password) => `
     <h1>Thank you for signing up!</h1>
     <p> your password is: ${password}</p>
     <p>Click <a href="http://localhost:3000">here</a> to log in.</p>
-`;
+    `;
 
 async function sendEmail(email, password) {
-    const transporter = nodeMailer.createTransport({
-        //use gmail
-        service: 'gmail',
-        auth: {
-            user: 'idanmeyer79@gmail.com',
-            pass: "app key"
-        }
-    })
+  const transporter = nodeMailer.createTransport({
+    //use gmail
+    service: "gmail",
+    auth: {
+      user: "printability2@gmail.com",
+      pass: process.env.GMAIL_APP_PASSWORD,
+    },
+  });
 
-    const info = await transporter.sendMail({
-        from: 'Printabillity <idan@tulu.io>',
-        to: email,
-        subject: 'Welcome to Printabillity',
-        html: html(password) 
-    })
-
+  const info = await transporter.sendMail({
+    from: "Printabillity <printability2@gmail.com>",
+    to: email,
+    subject: "Welcome to Printabillity",
+    html: html(password),
+    //add attachment
+    attachments: [
+      {
+        filename: "Printer setup Guide.pdf",
+        path: __dirname + "/../email attachments/Printer setup Guide.pdf",
+        contentType: "application/pdf",
+      },
+      {
+        filename: "CUPS server.zip",
+        path: __dirname + "/../email attachments/CUPS server.zip",
+        contentType: "application/zip",
+      },
+    ],
+  });
 }
 
 module.exports = { sendEmail };
