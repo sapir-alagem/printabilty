862c8da8a4a089090169003fcc0bff9a50de9e77
idanmeyer79 <idanmeyer79@gmail.com>
2024-08-31 21:01:16 +0300

    chore: Update AWS bucket region in S3 upload configuration

--- backend/services/s3Service.js
+++ backend/services/s3Service.js
@@ -1,11 +1,9 @@
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 exports.s3Uploadv3 = async (files) => {
-  console.log(
-    `certs: access key id: ${process.env.AWS_ACCESS_KEY_ID} ....... secrt: ${process.env.AWS_SECRET_ACCESS_KEY}`
-  );
+  console.log(`certs: region: ${process.env.AWS_REGION}`);
   const s3client = new S3Client({
-    region: process.env.AWS_REGION || "us-east-1",
+    region: process.env.AWS_REGION || "eu-north-1",
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
