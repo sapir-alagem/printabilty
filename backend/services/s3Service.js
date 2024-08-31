af58e1f939449dd7b4bd3537be35d915f2230be7
idanmeyer79 <idanmeyer79@gmail.com>
2024-08-31 20:40:03 +0300

    Refactor S3 upload logic to use AWS SDK directly

--- backend/services/s3Service.js
+++ backend/services/s3Service.js
@@ -1,34 +1,65 @@
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const AWS = require("aws-sdk");
 
 exports.s3Uploadv3 = async (files) => {
-  const s3client = new S3Client({ region: process.env.AWS_REGION });
-  const uploadedFiles = [];
+  try {
+    AWS.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    });
 
-  //handel single file upload
-  if (!Array.isArray(files)) {
-    files = [files];
-  }
+    const s3 = new AWS.S3();
 
-  console.log(files);
+    if (!Array.isArray(files)) {
+      files = [files];
+    }
 
-  for (const file of files) {
-    const uploadParams = {
+    const params = {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `uploads/${file.originalname}`,
-      Body: file.buffer,
+      Key: `uploads/${files[0].originalname}`,
+      Body: files[0].buffer,
       ACL: "public-read",
     };
 
-    try {
-      await s3client.send(new PutObjectCommand(uploadParams));
-      // Constructing the URL
-      const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${uploadParams.Key}`;
-      uploadedFiles.push(fileUrl);
-    } catch (error) {
-      console.error("Error uploading file to S3:", error);
-      throw error; // Rethrow the error to handle it in the controller
-    }
+    await s3.upload(params).promise();
+    const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${params.Key}`;
+    return fileUrl;
+  } catch (error) {
+    console.error("Error uploading file to S3:", error);
+    throw error;
   }
-
-  return uploadedFiles;
 };
+
+// exports.s3Uploadv3 = async (files) => {
+//   const s3client = new S3Client({ region: process.env.AWS_REGION });
+//   const uploadedFiles = [];
+
+//   //handel single file upload
+//   if (!Array.isArray(files)) {
+//     files = [files];
+//   }
+
+//   console.log(files);
+
+//   for (const file of files) {
+//     const uploadParams = {
+//       Bucket: process.env.AWS_BUCKET_NAME,
+//       Key: `uploads/${file.originalname}`,
+//       Body: file.buffer,
+//       ACL: "public-read",
+//     };
+
+//     try {
+//       await s3client.send(new PutObjectCommand(uploadParams));
+//       // Constructing the URL
+//       const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${uploadParams.Key}`;
+//       uploadedFiles.push(fileUrl);
+//     } catch (error) {
+//       console.error("Error uploading file to S3:", error);
+//       throw error; // Rethrow the error to handle it in the controller
+//     }
+//   }
+
+//   return uploadedFiles;
+// };
