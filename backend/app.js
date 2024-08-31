30c20597d5ce03a0462af209cbfd9fcdd911e5cf
sapir <sapir@almond-qms,com>
2024-08-31 19:12:01 +0300

    .

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,6 @@ const PrintJobsRoutes = require("./routes/print_jobs_routes.js");
 const companyRoutes = require("./routes/company_routes");
 const UploadRoutes = require("./routes/uploads_routes.js");
 const errorHandler = require("./controllers/errorHandler_controller.js");
-//verifyJWT is a middleware function that verifies the JWT token
 const verifyJWT = require("./middleWare/verifyJWT.js");
 const webhookRouter = require("./routes/webhookRouter.js");
 const qrCodesRouter = require("./routes/qrCodesRouter.js");
@@ -26,41 +25,30 @@ const credentials = require("./middleWare/credentials.js");
 const app = express();
 const server = http.createServer(app);
 
-//handle options credentials check before CORS and fetch cookies credentials requirements
 app.use(credentials);
-
-// CORS middleware
-
 app.use(cors(corsOptions));
-
-// Routes (ensure /webhook route comes before express.json())
 app.use("/webhook", webhookRouter);
-
-// Use body-parser middleware for other routes
 app.use(express.json());
-
-// Use cookie-parser middleware
 app.use(cookieParser());
 
-// Define other routes
 //if want to use JWT token verification, add app.use(verifyJWT) above the routes that need to be protected and uncomment the verifyJWT import
 app.use("/print_jobs", PrintJobsRoutes);
 app.use("/companies", companyRoutes);
 app.use("/uploads", UploadRoutes);
-app.use("/payment", paymentRoutes);
-app.use("/companies/:companyId/printers/:printerId/qrcodes", qrCodesRouter);
-app.use("/companies/:companyId", printerRouter);
-app.use("/register", registerRouter);
-app.use("/auth", authRouter);
-app.use("/refresh", refreshTokenRouter);
-app.use("/logout", logoutRouter);
+app.use('/payment', paymentRoutes);
+app.use('/companies/:companyId/printers/:printerId/qrcodes', qrCodesRouter);
+app.use('/companies/:companyId', printerRouter);
+app.use('/register', registerRouter);
+app.use('/auth', authRouter);
+app.use('/refresh', refreshTokenRouter);
+app.use('/logout', logoutRouter);
 
-// Error handler middleware
 app.use(errorHandler);
-
-// Setup WebSocket server
 setupWebSocketServer(server);
 
-// Start the server only if mongoDB is connected
+const port = process.env.PORT || 5000;
 
-server.listen(5000, () => console.log("Listening on port 5000"));
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}`);
+});
+
--- backend/middleWare/credentials.js
+++ backend/middleWare/credentials.js
@@ -2,9 +2,9 @@ const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
+  // if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", true);
-  }
+  // }
   next();
 };
 
