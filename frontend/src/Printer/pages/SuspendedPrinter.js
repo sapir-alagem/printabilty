8492f90e9d76835d1296692b65c0f98422630cfa
idanmeyer79 <idanmeyer79@gmail.com>
2024-09-02 14:10:56 +0300

    fix success and cancel payment pages

--- frontend/src/Printer/pages/SuspendedPrinter.js
+++ frontend/src/Printer/pages/SuspendedPrinter.js
@@ -3,8 +3,19 @@ import React from "react";
 const PrinterSuspended = () => {
   return (
     <div className="container text-center">
-      <h1>Printer Suspended</h1>
-      <p>We're sorry, but the printer you are trying to access has been suspended.</p>
+      <h1>
+        Printer Suspended{" "}
+        <span role="img" aria-label="sad face">
+          😔
+        </span>
+        <span role="img" aria-label="pritner">
+          🖨️
+        </span>
+      </h1>
+      <p>
+        We're sorry, but the printer you are trying to access has been
+        suspended.
+      </p>
       <p>Please try a different printer</p>
     </div>
   );
--- frontend/src/pages/SuccessPage.js
+++ frontend/src/pages/SuccessPage.js
@@ -1,20 +1,34 @@
+import { Button } from "@mui/material";
 import React, { useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const SuccessPage = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   return (
     <div className="container mt-5">
       <div className="text-center">
-        <h1 className="display-4">Payment Successful</h1>
+        <h1 className="display-4">
+          Payment Successful{" "}
+          <span role="img" aria-label="smily face">
+            😊
+          </span>
+        </h1>
         <p className="lead">
-          Thank you for your payment. Your transaction was successful
+          Thank you for your payment! Your transaction was successful. you can
+          collect your print job from the printer.
         </p>
-        <Link to="/" className="btn btn-success mt-3">
-          Go to Home
-        </Link>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => {
+            navigate(-4);
+          }}
+        >
+          print another document
+        </Button>
       </div>
     </div>
   );
